60f37bfb078102faa80affe54728f4ef30dcc59c
Pradhuman <49186033+gaulghost@users.noreply.github.com>
2023-12-03 12:15:22 +0530

    Time: 4 ms (78.97%), Space: 10.7 MB (5.92%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
@@ -3,12 +3,9 @@ public:
     int minTimeToVisitAllPoints(vector<vector<int>>& points) {
         int ans = 0, i = points[0][0], j = points[0][1];
         for(vector<int> point: points){
-            int i_new = point[0], j_new = point[1];
-            int diagonal = min(abs(i-i_new), abs(j-j_new));
-            int ver_hoz = max(abs(i-i_new), abs(j-j_new)) - diagonal;
-            ans += diagonal + ver_hoz;
-            i = i_new;
-            j = j_new;
+            ans += max(abs(i-point[0]), abs(j-point[1]));
+            i = point[0];
+            j = point[1];
         }
         return ans;
     }
