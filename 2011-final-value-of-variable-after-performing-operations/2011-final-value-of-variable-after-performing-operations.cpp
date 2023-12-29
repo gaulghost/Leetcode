513aeb0627f5f514e260d8ba8f66c17460c8b3d1
Pradhuman <49186033+gaulghost@users.noreply.github.com>
2023-12-29 21:55:41 +0530

    Time: 17 ms (13.38%), Space: 14.4 MB (20.73%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -2,41 +2,17 @@ class Solution {
 public:
     int finalValueAfterOperations(vector<string>& operations) {
         int result = 0;
-        for (int i=0; i<operations.size(); i++){
-            if (operations[i] == "--X") {
-                result = result - 1;
-            } else if (operations[i] == "X--") {
-                result = result - 1;
-            } else if (operations[i] == "++X") {
-                result = result + 1;
+        for (string x : operations){
+            if (x == "--X") {
+                result -= 1;
+            } else if (x == "X--") {
+                result -= 1;
+            } else if (x == "++X") {
+                result += 1;
             } else {
-                result = result + 1;
+                result += 1;
             }
         }
         return result;
     }
 };
-
-// coming in a min
-
-// result = 0
-// operations = ["--X","X++","X++"] 
-// operations.size() -> 3
-
-// int =0; 0<3; 0++
-// operations[0] -> "--X"
-// operations[i] == "--X" -> true
-// result = result - 1;
-// result = -1
-
-// int = 1; 1<3; 1++
-// operations[1] -> "X++"
-// operations[i] == "--X" -> false
-
-// int = 2; 2<3; 2++
-// operations[2] -> "X++"
-// operations[i] == "--X" -> false
-
-// int =3; 3<3, exit loop
-
-// return result, result -> -1
