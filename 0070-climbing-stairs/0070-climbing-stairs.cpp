3f4f605fec60795bb484f47c1c37a243002c3673
Pradhuman <49186033+gaulghost@users.noreply.github.com>
2025-09-06 10:07:46 +0530

    Time: 0 ms (100%), Space: 7.7 MB (87.76%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int climbStairs(int n) {
+        if(n==1) return 1;
         int v1=1, v2=2;
-        if(n==1) return v1;
-        if(n==2) return v2;
         for(int i=3; i<n+1; i++){
             int v3 = v2;
             v2 = v1+v2;
