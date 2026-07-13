8fe77e74909fcf67cff59d9429036e0e8219818c
Pradhuman <49186033+gaulghost@users.noreply.github.com>
2026-07-13 14:33:28 +0530

    Added solution - LeetHub

---
+++ 704474-while-loop-GFG/704474-while-loop.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    void utility(int x) {
+        // Use a while loop to process 'x' until condition is met
+        // Example approach: Initialize a counter, loop while counter > 0, decrement inside loop
+        int count = x;
+        while (count >= 0) {
+            // Process here
+            cout<<count;
+            if(count != 0) cout<<" ";
+            count--;
+        }
+        // Time Complexity: O(n) where n is the initial value of 'x'
+        // Space Complexity: O(1) - constant space used
+        // Optimal solution may exist depending on problem constraints
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
