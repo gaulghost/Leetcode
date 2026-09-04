c4f28f7559d405fa2034f7315c5afec01786b50a
Pradhuman <49186033+gaulghost@users.noreply.github.com>
2026-09-05 02:20:29 +0530

    Time: 29 ms (40.93%), Space: 16.2 MB (90.29%) - LeetHub - Time: O(M*N), Space: O(M*N)

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    void markIslandVisited(int i, int j, vector<vector<int>>& map, vector<vector<char>>& grid){
-        if(grid[i][j] == '0') return;
-        if(map[i][j] == 1) return;
-        map[i][j] = 1;
-        int g_sz = grid.size(), gl_sz = grid[0].size();
-        if(i+1 < g_sz && grid[i+1][j] == '1') markIslandVisited(i+1,j,map,grid);
-        if(i-1 >= 0 && grid[i-1][j] == '1') markIslandVisited(i-1,j,map,grid);
-        if(j+1 < gl_sz && grid[i][j+1] == '1') markIslandVisited(i,j+1,map,grid);
-        if(j-1 >= 0 && grid[i][j-1] == '1') markIslandVisited(i,j-1,map,grid);
+    void markIslandVisited(int i, int j, vector<vector<char>>& grid){
+        if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size() || grid[i][j] == '0')
+            return;
+        grid[i][j] = '0';
+        markIslandVisited(i+1,j,grid);
+        markIslandVisited(i-1,j,grid);
+        markIslandVisited(i,j+1,grid);
+        markIslandVisited(i,j-1,grid);
     }
 
     int numIslands(vector<vector<char>>& grid) {
-        int totalIslands = 0;
-        vector<vector<int>>map(grid.size(), vector<int>(grid[0].size(),0));
+        int ans = 0;
         for(int i=0; i<grid.size(); i++){
-            for (int j=0; j<grid[0].size(); j++){
-                if(grid[i][j] == '1' and map[i][j] == 0){
-                    markIslandVisited(i, j, map, grid);
-                    totalIslands++;
+            for(int j=0; j<grid[0].size(); j++){
+                if(grid[i][j] == '1'){
+                    markIslandVisited(i, j, grid);
+                    ans++;
                 }
             }
         }
-        return totalIslands;
+        return ans;
     }
 };
 
