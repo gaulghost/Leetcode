e46206c90f50a6b51082ef8b6fd83d0c9029b742
Pradhuman <49186033+gaulghost@users.noreply.github.com>
2026-09-05 11:11:46 +0530

    Time: 0 ms (100%), Space: 16.8 MB (69.11%) - LeetHub - Time: O(m * n), Space: O(m * n)

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,48 +1,44 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        queue<pair<int,int>> rotten;
-        int farthestOrangeDist = 0, cnt = 0;
-        for(int i =0; i<grid.size(); i++){
+        queue<pair<int,int>> rot;
+        int tot = 0;
+        for(int i=0; i<grid.size(); i++){
             for(int j=0; j<grid[0].size(); j++){
-                if(grid[i][j] == 2){
-                    rotten.push({i,j});
-                }
-                if(grid[i][j] == 1) cnt++;
+                if(grid[i][j] == 2) rot.push({i,j});
+                if(grid[i][j] == 1) tot++;
             }
         }
-        if(cnt == 0) return 0;
-        while(!rotten.empty()){
-            int sz = rotten.size();
+        if(tot == 0) return 0;
+        int time = 0;
+        while(!rot.empty()){
+            int sz = rot.size();
             for(int k=0; k<sz; k++){
-                int i = rotten.front().first;
-                int j = rotten.front().second;
-                rotten.pop();
-                if(i+1<grid.size() && grid[i+1][j] == 1){
-                    rotten.push({i+1, j});
-                    grid[i+1][j] = 2;
-                }
-                if(i>0 && grid[i-1][j] == 1){
-                    rotten.push({i-1, j});
+                auto [i, j] = rot.front(); rot.pop();
+                if(i>0 && grid[i-1][j]==1){
                     grid[i-1][j] = 2;
+                    rot.push({i-1, j});
+                    tot--;
                 }
-                if(j+1<grid[0].size() && grid[i][j+1] == 1){
-                    rotten.push({i, j+1});
-                    grid[i][j+1] = 2;
+                if(i+1<grid.size() && grid[i+1][j]==1){
+                    grid[i+1][j] = 2;
+                    rot.push({i+1, j});
+                    tot--;
                 }
-                if(j>0 && grid[i][j-1] == 1){
-                    rotten.push({i, j-1});
+                if(j>0 && grid[i][j-1]==1){
                     grid[i][j-1] = 2;
+                    rot.push({i, j-1});
+                    tot--;
+                }
+                if(j+1<grid[0].size() && grid[i][j+1]==1){
+                    grid[i][j+1] = 2;
+                    rot.push({i, j+1});
+                    tot--;
                 }
             }
-            farthestOrangeDist++;
+            time++;
         }
-        for(int i =0; i<grid.size(); i++){
-            for(int j=0; j<grid[0].size(); j++){
-                if(grid[i][j] == 1) return -1;
-            }
-        }
-        return farthestOrangeDist-1;
+        return tot == 0 ? time-1 : -1;
     }
 };
 
